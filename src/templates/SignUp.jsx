5c6b42a2e0cda91f85e663db54f719b8737dee32
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-07-30 17:00:49 +0900

    role属性追加

--- src/templates/SignUp.jsx
+++ src/templates/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useState} from 'react';
-import {TextInput, PrimaryButton} from '../components/UIkit';  //コンポーネント
+import {TextInput, PrimaryButton, SelectBox} from '../components/UIkit';  //コンポーネント
 import {signUp} from '../reducks/users/operations';  //signup関数
 import {useDispatch} from 'react-redux';
 import {push} from 'connected-react-router';
@@ -13,7 +13,8 @@ const SignUp = () => {
   const [username, setUsername] = useState(""),
         [email, setEmail] = useState(""),
         [password, setPassword] = useState(""),
-        [confirmPassword, setConfirmPassword] = useState("");
+        [confirmPassword, setConfirmPassword] = useState(""),
+        [role, setRole] = useState("");
 
   const inputUsername = useCallback((event) => {
     setUsername(event.target.value)
@@ -31,35 +32,49 @@ const SignUp = () => {
     setConfirmPassword(event.target.value)
   }, [setConfirmPassword])
 
+  const roles = [
+    {id: 'customer', name: 'ユーザー'},
+    {id: 'shop', name: 'ショップ'},
+  ];
+
   return (
-    <div className="c-section-container">
-      <h2 className="u-text__headline u-text-center">アカウント登録</h2>
-      <div className="module-spacer--medium" />
-      <TextInput
-        fullWidth={true} label={'ユーザー名'} multiline={false} required={true}
-        rows={1} value={username} type={'text'} onChange={inputUsername}
-      />
-      <TextInput
-        fullWidth={true} label={'メールアドレス'} multiline={false} required={true}
-        rows={1} value={email} type={'email'} onChange={inputEmail}
-      />
-      <TextInput
-        fullWidth={true} label={'パスワード'} multiline={false} required={true}
-        rows={1} value={password} type={'password'} onChange={inputPassword}
-      />
-      <TextInput
-        fullWidth={true} label={'パスワード(再確認)'} multiline={false} required={true}
-        rows={1} value={confirmPassword} type={'password'} onChange={inputConfirmPassword}
-      />
-      <div className="module-spacer--medium" />
-      <div className="center">
-        <PrimaryButton
-          label={'アカウントを作成する'}
-          onClick={() => dispatch(signUp(username, email, password, confirmPassword))}
-        />
-        {/* 入力された値を引数で渡してsignup関数 */}
+    <div className="c-section-gray">
+      <div className="c-section-container">
+        <h2 className="u-text__headline u-text-center">アカウント登録</h2>
         <div className="module-spacer--medium" />
-        <p onClick={() => dispatch(push('/signin'))}>アカウントをお持ちの方はこちら</p>
+        <TextInput
+          fullWidth={true} label={'ユーザー名'} multiline={false} required={true}
+          rows={1} value={username} type={'text'} onChange={inputUsername}
+        />
+        <div className="module-spacer--extra-small"/>
+        <TextInput
+          fullWidth={true} label={'メールアドレス'} multiline={false} required={true}
+          rows={1} value={email} type={'email'} onChange={inputEmail}
+        />
+        <div className="module-spacer--extra-small"/>
+        <TextInput
+          fullWidth={true} label={'パスワード'} multiline={false} required={true}
+          rows={1} value={password} type={'password'} onChange={inputPassword}
+        />
+        <div className="module-spacer--extra-small"/>
+        <TextInput
+          fullWidth={true} label={'パスワード(再確認)'} multiline={false} required={true}
+          rows={1} value={confirmPassword} type={'password'} onChange={inputConfirmPassword}
+        />
+        <div className="module-spacer--extra-small"/>
+        <SelectBox
+          label={'どちらで利用するか'} required={true} options={roles} select={setRole} value={role}
+        />
+        <div className="module-spacer--medium" />
+        <div className="center">
+          <PrimaryButton
+            label={'アカウントを作成する'}
+            onClick={() => dispatch(signUp(username, email, password, confirmPassword, role))}
+          />
+          {/* 入力された値を引数で渡してsignup関数 */}
+          <div className="module-spacer--medium" />
+          <p  className="text-pointer" onClick={() => dispatch(push('/signin'))}>アカウントをお持ちの方はこちら</p>
+        </div>
       </div>
     </div>
   )
