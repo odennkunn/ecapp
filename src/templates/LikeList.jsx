27c0e7761cf9f8ff5c4992e2b88ca566e3c7b565
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-08-03 17:15:49 +0900

    詳細ページに遷移できるよう修正

--- src/templates/LikeList.jsx
+++ src/templates/LikeList.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {getUserLikes} from '../reducks/users/selectors';
+import {ProductCard} from '../components/Products/';
 
 
 const LikeList = () => {
 
+  const selector = useSelector((state) => state); //storeのstate全体
+  const likes = getUserLikes(selector);
+
+
   return (
     <section className="c-section-wrapin">
-      <p>いいね リスト</p>
+      <h2 className="list-h">お気に入り商品</h2>
+      <div className="p-grid__row">
+        {likes.length > 0 && (  //商品が一つ以上登録されているとき
+          likes.map(product => (  //mapで商品一つ一つを回して、productcardに渡す
+            <ProductCard
+              key={product.LikesId} id={product.productId} name={product.name}
+              images={product.images} price={product.price}
+            />
+          ))
+        )}
+      </div>
     </section>
   )
 }
