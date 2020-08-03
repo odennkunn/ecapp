74462b78154a2f7f81320cf4dd86533b506c7274
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-08-03 15:40:22 +0900

    仮ページ作成

---
+++ src/templates/LikeList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const LikeList = () => {
+
+  return (
+    <section className="c-section-wrapin">
+      <p>いいね リスト</p>
+    </section>
+  )
+}
+
+export default LikeList
